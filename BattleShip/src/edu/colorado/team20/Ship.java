504a217f016cd44aa5bd0702ba6fdab160df7743
sapa3451 <sapa3451@colorado.edu>
2021-02-14 14:33:54 -0700

    initial tests

--- BattleShip/src/edu/colorado/team20/Ship.java
+++ BattleShip/src/edu/colorado/team20/Ship.java
@@ -2,15 +2,26 @@ package edu.colorado.team20;
 // This is the  baseclass for your ship.  Modify accordingly
 // TODO: practice good OO design
 public class Ship {
-    Ship () {
 
-        //Team20, pair 1 was here.
+    private String shipName;
+    private int shipSize;
+
+    public Ship(String shipName, int shipSize) {
+
+        this.shipName = shipName;
+        this.shipSize = shipSize;
 
         // Team 20, pair 2 was here.
 
     }
 
-    private String name;
+    public String getShipName() {
+        return shipName;
+    }
+
+    public int getShipSize() {
+        return shipSize;
+    }
     // TODO: create appropriate getter and setter methods
     // TODO: Understand encapsulation
     // TODO: Understand what these todo comments mean
--- BattleShip/test/test/ShipTest.java
+++ BattleShip/test/test/ShipTest.java
@@ -1,7 +1,16 @@
 package test;
 
+import edu.colorado.team20.Ship;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShipTest {
+    @Test
+    public void GetShipNameAndSize () {
+        Ship testShip1 = new Ship("Destroyer", 4);
+        assertEquals("Destroyer", testShip1.getShipName());
+        assertEquals(4, testShip1.getShipSize());
+    }
 
 }
